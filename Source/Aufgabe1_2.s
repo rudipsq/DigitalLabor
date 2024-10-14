d003b9be6235a7ecd37a2ecd99c839f9d00e90c3
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-10-14 11:34:54 +0200

    aufgabe 2,3

--- Source/Aufgabe1_2.s
+++ Source/Aufgabe1_2.s
@@ -12,6 +12,30 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+  /* a */
+  mov r0,#4294967295
+  mov r1,#1
+  add r10,r0,r1
+
+
+  /* b */
+   mov r0,#0xffffffff
+   mov r1,#1
+   add r11,r0,r1
+
+   mov r0,#~0
+   mov r1,#1
+   add r11,r0,r1
+
+
+   /* c */
+   mov r0,#2147483648
+   mov r1,#2147483648
+   add r11,r0,r1
+
+   mov r0,#0x80000000
+   mov r1,#0x80000000
+   add r11,r0,r1
 
 
 stop:
--- Source/Aufgabe1_3.s
+++ Source/Aufgabe1_3.s
@@ -12,6 +12,25 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+  mov r0,#255 /* Threashold */
+  mov r1,#256   /* Wert */
+
+  subs r10, r0, r1
+
+  bcc ifbigger
+  mov r10, #0
+
+  b ifend
+
+
+
+  ifbigger:
+  mov r10, #255
+
+  ifend:
+
+
+
 
 stop:
 	nop
