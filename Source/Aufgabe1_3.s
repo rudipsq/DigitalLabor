13ac73ecdeea96e4b8111e3247d4778d08b41dbb
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-10-15 08:17:18 +0200

    update

--- Source/Aufgabe1_3.s
+++ Source/Aufgabe1_3.s
@@ -12,6 +12,7 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+/* version 1 mit bed. sprung */
   mov r0,#255 /* Threashold */
   mov r1,#256   /* Wert */
 
@@ -28,6 +29,23 @@ main:
   mov r10, #255
 
   ifend:
+
+
+
+  /* reset... */
+  mov r0,#0
+  mov r1,#0
+  mov r10,#0
+
+
+  /* version 2 mit bed. ausfuehrung */
+  mov r0,#255 /* Threashold */
+  mov r1,#255   /* Wert */
+
+  subs r10, r0, r1
+
+  mov r10,#0
+  movcc r10, #255
 
 
 
--- Source/Aufgabe1_4.s
+++ Source/Aufgabe1_4.s
@@ -13,6 +13,26 @@
 .global main /* Specify global symbol */
 main:
 
+ /* Zahlen */
+  mov r0, #0xffffffff
+  mov r1, #0xffffffff
+  mov r2, #10
+  mov r3, #100
+
+  /* Ergebnisregister */
+  mov r4, #0
+  mov r5, #0
+  mov r6, #0
+
+
+   /* Rechnung */
+  adds r4, r0, r2
+
+  adcs r5, r1, r3
+
+  adccs r6, r6, #0
+
+
 
 stop:
 	nop
